1df3bacb4ca5f3a0e33728c2db4e98f502383eff
sm823zw <saankhya1997@gmail.com>
2020-09-21 23:49:13 +0530

    Created new files

---
+++ includeAllLibraries.R
@@ -0,0 +1,15 @@
+includeAllLibraries <- function(){
+  #install.packages("ggplot2")
+  #install.packages("dplyr")
+  #install.packages("reshape")
+  #install.packages("plyr")
+  #install.packages("zoo")
+  #install.packages("ggrepel")
+  
+  library(ggplot2)
+  library(dplyr)
+  library(reshape)
+  library(plyr)
+  library(zoo)
+  library(ggrepel)
+}
